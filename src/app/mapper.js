11ca9e8909433e39288adffcb426fb7fb590b829
SengulC <sengulcagdal@gmail.com>
2025-01-01 17:27:10 +0200

    mapping!

---
+++ src/app/mapper.js
@@ -0,0 +1,28 @@
+// import ImageMapper from "react-img-mapper";
+
+// const Mapper = (props) => {
+//   const URL =
+//     "https://raw.githubusercontent.com/naknamu/wheres-waldo/main/src/assets/images/extended_city.jpg";
+//   const MAP = {
+//     name: "my-map",
+//     areas: 
+//         {
+//           "id": "469f9800-c45a-483f-b13e-bd24f3fb79f4",
+//           "title": "drink",
+//           "shape": "rect",
+//           "name": "1",
+//           "fillColor": "#eab54d4d",
+//           "strokeColor": "black",
+//           "coords": [462, 292, 486, 335]
+//         },
+//   };
+
+//   return (
+//     <ImageMapper
+//       src={URL}
+//       map={MAP}
+//     />
+//   );
+// };
+
+// export default Mapper;
--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,36 @@
-'use client'
+"use client"
+import ImageMapper from "react-img-mapper";
+import areasFile from "./maps.json"
 
 export default function Home() {
+  const areas = areasFile;
+  // const areas = [
+  //   { id: "1", shape: "circle", coords: [150, 150, 50] },
+  //   { id: "2", shape: "rect", coords: [300, 200, 400, 300] }
+  // ];
   return (
     <div>
-    <map name="foodmap">
-      <area alt="drink" title="drink" href="google.com" coords="462,292,486,335" shape="rect"/>
-      <area alt="cookies" title="cookies" href="google.com" coords="572,323,19" shape="circle"/>
-      <area alt="curry" title="curry" href="google.com" coords="882,267,22" shape="circle"/>
-    </map>
-      <img useMap="#foodmap" className="home" src="home.jpg"></img>
+      <ImageMapper
+            map={{
+                name:"my-map", 
+                areas: areas
+                }}
+            src={'home.jpg'}
+            active={true}
+            onClick={(e) => {
+              console.log("Hello!");
+            }}
+            // responsive
+          />
     </div>
-);
+  );
 }
+
+    // <div>
+    // <map name="foodmap">
+    //   <area alt="drink" title="drink" href="https://stormboard.vercel.app/" target="_blank" coords="462,292,486,335" shape="rect"/>
+    //   <area alt="cookies" title="cookies" href="https://stormboard.vercel.app/" target="_blank" coords="572,323,19" shape="circle"/>
+    //   <area alt="curry" title="curry" href="https://stormboard.vercel.app/" target="_blank" coords="882,267,22" shape="circle"/>
+    // </map>
+    //   <img useMap="#foodmap" className="home" src="home.jpg"></img>
+    // </div>
