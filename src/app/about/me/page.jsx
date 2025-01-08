fa70f55b1b6110c8069ac69bbb241d9b912abbc5
SengulC <sengulcagdal@gmail.com>
2025-01-09 00:11:42 +0200

    css

--- src/app/about/me/page.jsx
+++ src/app/about/me/page.jsx
@@ -10,12 +10,16 @@ export default function AboutMe() {
         <Link className="link" href="/about/credits"> &gt; Credits </Link>
         <div className='aboutme'>
             <img src="/avatar.png"></img>
-            <p>
-                Hi! I am a bad bitch and I made this website!
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi repellat velit, est, nobis porro dignissimos deserunt eum cum, excepturi corrupti molestiae optio earum. Delectus blanditiis consequatur libero magnam odit exercitationem!
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel harum quis deleniti cumque hic modi consectetur magnam molestias nulla, saepe minima esse eveniet voluptas consequuntur officia quidem doloribus accusamus quasi?
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam placeat exercitationem quas ipsa enim nihil magni illum voluptates sequi! Tenetur sint nihil delectus corrupti veniam necessitatibus nobis, ab magni assumenda.
-            </p>
+            <div className='aboutmeTxt'>
+                <h2>A little about me...</h2>
+                <p>
+                    Hi! I am a bad bitch and I made this website!
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi repellat velit, est, nobis porro dignissimos deserunt eum cum, excepturi corrupti molestiae optio earum. Delectus blanditiis consequatur libero magnam odit exercitationem!
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel harum quis deleniti cumque hic modi consectetur magnam molestias nulla, saepe minima esse eveniet voluptas consequuntur officia quidem doloribus accusamus quasi?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam placeat exercitationem quas ipsa enim nihil magni illum voluptates sequi! Tenetur sint nihil delectus corrupti veniam necessitatibus nobis, ab magni assumenda.
+                </p>
+                <h2>How to interact with this website</h2>
+            </div>
         </div>
     </body>
   )
