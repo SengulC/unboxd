3eaae0a796524189737deb3d4ea8a0229ee2e148
SengulC <sengulcagdal@gmail.com>
2025-01-12 17:18:21 +0200

    new homepage, icon, and links

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,6 @@
 import ImageMapper from "react-img-mapper";
 import areasFile from "/public/JSON/imageMapCoords.json"
 import { useRouter } from 'next/navigation'
-import Link from "next/link";
 
 export default function Home() {
   const router = useRouter();
@@ -15,14 +14,16 @@ export default function Home() {
                 name:"my-map", 
                 areas: areas
                 }}
-            src={'background/home2.png'}
+            src={'background/homepage.png'}
             active={true}
+            responsive={true}
+            parentWidth={900}
+            width={900}
             onClick={(e) => {
-              router.push('/rooms/cinema');
+              router.push(e.title);
             }}
           />
       {/* <div className="fuzzy-overlay"></div> */}
-      <Link id="creditsLink" className="link" href="/about/me"> ? </Link>
     </body>
   );
 }
