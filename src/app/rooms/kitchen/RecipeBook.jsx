6094538bd9afb5437dd367421d7452ec7cf12d00
SengulC <sengulcagdal@gmail.com>
2025-01-14 17:34:11 +0200

    book text

--- src/app/rooms/kitchen/RecipeBook.jsx
+++ src/app/rooms/kitchen/RecipeBook.jsx
@@ -21,7 +21,7 @@ const Page = React.forwardRef((props, ref) => {
 return (
   <div className={`page ${props.side}`} data-density="hard" ref={ref}>
     <div className="page-content">
-      <h2 className="page-header">Page header - {props.number}</h2>
+      <h2 className="page-header">{props.header}</h2>
       <div className="page-image"></div>
       <div className="page-text">{props.children}</div>
       <div className="page-footer">{props.number + 1}</div>
@@ -54,11 +54,32 @@ export default function RecipeBook() {
       size="stretch"
       maxShadowOpacity={0}
       >
-        <PageCover side={"L"}> Recipes </PageCover>
-        <Page side={"R"} number={1}>Lorem ipsum...</Page>
-        <Page side={"L"} number={2}>Lorem ipsum...</Page>
-        <Page side={"R"} number={3}>PAGE 2 ipsum...</Page>
-        <Page side={"L"} number={4}>Lorem ipsum...</Page>
+        <PageCover side={"L"}> Desserts </PageCover>
+        <Page side={"R"} number={1} header={"Fl-Earl Gray Cake"}>
+          Earl gray and marzipan-chunks sponge cake (1:1:1 b:s:f ratio, 3 eggs) soaked with çiçek suyu and rose syrup. <br/>
+          Rose cream cheese frosting. <br/>
+          Rose marmalade to decorate. <br/>
+          Heavily inspired by <a href="https://theclovecoterie.substack.com/p/nosferatu-cake">Nosferatu cake.</a>
+        </Page>
+        <Page side={"L"} number={2} header={"White C(h)oconut Cookies"}>
+          Butter + Coconut oil-based cookies.
+          Recipe <a href="https://www.crunchycreamysweet.com/white-chocolate-coconut-cookies-recipe/">here.</a>
+        </Page>
+        <Page side={"R"} number={3} header={"Triple Velvet Cupcakes"}>
+          Red velvet cupcakes with cherries, chocolate chunks and beetroot. <br/>
+          Follow classic red velvet with beetroot (gives both color and gooeyness) recipe. <br/>
+          Add milk chocolate chunks and frozen cherries as desired to batter. Bake. <br/>
+          Decorate with cream cheese frosting spiked with bitter almond syrup 
+          (gives that Dr Pepper-esque fake cherry taste!) and frozen cherries.
+          Always make sure cream cheese and butter is room temperature before making the frosting.
+        </Page>
+        <PageCover side={"L"}> Savory Sauces </PageCover>
+        <Page side={"R"} number={4} header={"Steak Garlic Butter"}>
+          Lorem ipsum...
+        </Page>
+        <Page side={"L"} number={5} header={"Burger Sauce"}>
+          Lorem ipsum...
+        </Page>
         <PageCover side={"R"}> Fin. </PageCover>
       </HTMLFlipBook>
     </div>
--- src/app/rooms/kitchen/page.jsx
+++ src/app/rooms/kitchen/page.jsx
@@ -1,5 +1,5 @@
 "use client"
-import { React, useState } from "react";
+import { React, useEffect, useState } from "react";
 import ImageMapper from "react-img-mapper";
 import { useRouter } from 'next/navigation'
 import coords from "/public/JSON/coords.js";
@@ -8,28 +8,84 @@ import RecipeBook from "./RecipeBook";
 export default function Kitchen() {
   const router = useRouter();
   const [showBook, setShowBook] = useState(true);
+  const [kitchenCoords, setKitchenCoords] = useState(coords.kitchenCoords);
 
+  // useEffect(() => {
+  //   setKitchenCoords([
+  //   {
+  //   "id": "homeButton",
+  //   "title": "/",
+  //   "shape": "rect",
+  //   "fillColor": "rgba(0,0,0,0)",
+  //   "strokeColor": "rgba(0,0,0,0)",
+  //   "lineWidth": 2.5,
+  //   "coords": [530,738,586,791]
+  //   },
+  //   {
+  //   "id": "aboutButton",
+  //   "title": "/about/me/",
+  //   "shape": "rect",
+  //   "fillColor": "rgba(0,0,0,0)",
+  //   "strokeColor": "rgba(0,0,0,0)",
+  //   "lineWidth": 2.5,
+  //   "coords": [433,737,485,790]
+  //   },
+  //   {
+  //   "id": "creditsButton",
+  //   "title": "/about/credits/",
+  //   "shape": "rect",
+  //   "fillColor": "rgba(0,0,0,0)",
+  //   "strokeColor": "rgba(0,0,0,0)",
+  //   "lineWidth": 2.5,
+  //   "coords": [633,736,686,793]
+  //   },
+  //   {
+  //   "id": "book",
+  //   "title": "/rooms/kitchen/recipebook/",
+  //   "shape": "rect",
+  //   "fillColor": "rgba(0,0,0,0)",
+  //   "strokeColor": "rgba(0,0,0,0)",
+  //   "lineWidth": 2.5,
+  //   "coords": [700,557,831,676]
+  //   },
+  //   {
+  //   "id": "cake",
+  //   "title": "/rooms/kitchen/recipebook?pageTo=2",
+  //   "shape": "circle",
+  //   "fillColor": "rgba(0,0,0,0)",
+  //   "strokeColor": "rgba(0,0,0,0)",
+  //   "lineWidth": 2.5,
+  //   "coords": [421,268,20]
+  //   }
+  // ]);
+  // }, [kitchenCoords])
+
+  useEffect(() => {
+    setKitchenCoords(coords.kitchenCoords);
+  }, [])
+  
+  
   return (
   <body className="kitchen">
     <div className="tvbgContainer">
       <ImageMapper className="tvbg"
               map={{
                   name:"my-map", 
-                  areas: coords.kitchenCoords
+                  areas: kitchenCoords
                   }}
               src={'/background/kitchenpage.png'}
               active={true}
               responsive={true}
               parentWidth={900}
+              length={900}
               width={900}
               onClick={(e) => {
                 // alert(e.title)
-                setShowBook(true);
+                setShowBook(false);
                 // router.push(e.title)
               }}
             />
     </div>
-    
     {showBook && 
       <RecipeBook/>
     }
