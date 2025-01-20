5d8067bcc0789fdd7c1171daab1eec68e1c4e4b5
SengulC <sengulcagdal@gmail.com>
2025-01-20 16:16:22 +0200

    wrapped push to router for movie/page.jsx in suspense

--- src/app/rooms/cinema/Movie.jsx
+++ src/app/rooms/cinema/Movie.jsx
@@ -1,19 +1,22 @@
 "use client"
 
-import React from 'react'
+import React, { Suspense } from 'react'
 import { useRouter } from 'next/navigation'
 
 export function MoviePoster({movieTitle, src}) {
     const router = useRouter(); 
     return (
     <div className='MoviePoster'>
+        <Suspense>
         <img 
             onClick={(e)=>router.push(
             `/rooms/cinema/movie?title=${movieTitle}`
             )} 
             className='moviePoster' 
             src={src}>
-        </img><br/>
+        </img>
+        </Suspense>
+        <br/>
     </div>
     )
 }
