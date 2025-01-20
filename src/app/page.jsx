735bdd42ef81fd7ee181f05ee81651742d4f2da8
SengulC <sengulcagdal@gmail.com>
2025-01-20 16:46:04 +0200

    big mask behind image

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,27 +2,44 @@
 import ImageMapper from "react-img-mapper";
 import coords from "/public/JSON/coords.js";
 import { useRouter } from 'next/navigation'
+import { motion } from 'framer-motion';
+import useMousePosition from './useMousePosition';
+import { useState } from "react";
 
 export default function Home() {
   const router = useRouter();
+  const [isHovered, setIsHovered] = useState(false);
+  const { x, y } = useMousePosition();
+  const size = isHovered ? 800 : 40;
 
   return (
     <body className="home">
-      <ImageMapper
-            map={{
-                name:"my-map", 
-                areas: coords.homeCoords
-                }}
-            src={'background/homepage.png'}
-            active={true}
-            responsive={true}
-            parentWidth={900}
-            width={900}
-            onClick={(e) => {
-              router.push(e.title);
-            }}
-          />
-      {/* <div className="fuzzy-overlay"></div> */}
+      <motion.div 
+        className={"mask"}
+        animate={{
+          WebkitMaskPosition: `${x - (size/2)}px ${y - (size/2)}px`,
+          WebkitMaskSize: `${size}px`,
+        }}
+        transition={{ type: "tween", ease: "backOut", duration:0.5}}
+      />
+      <div className="tvbgContainer">
+        <ImageMapper
+              map={{
+                  name:"my-map", 
+                  areas: coords.homeCoords,
+                  className: "mapster"
+                  }}
+              src={'background/homepage.png'}
+              active={true}
+              responsive={true}
+              parentWidth={900}
+              width={900}
+              onClick={(e) => {
+                router.push(e.title);
+              }}
+              onMouseEnter={() => {setIsHovered(true)}} onMouseLeave={() => {setIsHovered(false)}}
+            />
+        </div>
     </body>
   );
 }
