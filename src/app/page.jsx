066c41098bcfdbf52f5b589e5e7dba0b8743007c
SengulC <sengulcagdal@gmail.com>
2025-01-26 17:54:09 +0200

    got rid of unnecess stuff

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,20 +5,8 @@ import { useRouter } from 'next/navigation'
 
 function Home () {
   const router = useRouter();
-  // const [isHovered, setIsHovered] = useState(false);
-  // const { x, y } = useMousePosition();
-  // const size = isHovered ? 800 : 40;
-
   return (
     <div className="home">
-      {/* <motion.div 
-        className={"mask"}
-        animate={{
-          WebkitMaskPosition: `${x - (size/2)}px ${y - (size/2)}px`,
-          WebkitMaskSize: `${size}px`,
-        }}
-        transition={{ type: "tween", ease: "backOut", duration:0.5}}
-      /> */}
         <ImageMapper
           map={{
               name:"my-map", 
@@ -32,7 +20,6 @@ function Home () {
           onClick={(e) => {
             router.push(e.title);
           }}
-          // onMouseEnter={() => {setIsHovered(true)}} onMouseLeave={() => {setIsHovered(false)}}
             />
     </div>
   );
