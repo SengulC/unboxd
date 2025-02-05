4eea181a4dd8a00fce1f0898f725b38b0a972d21
SengulC <sengulcagdal@gmail.com>
2025-02-05 18:11:47 +0200

    game popup

--- src/app/rooms/games/page.jsx
+++ src/app/rooms/games/page.jsx
@@ -3,13 +3,21 @@ import Image from 'next/image';
 import WindowsBtn from './WindowsBtn';
 import React, { useEffect, useState } from 'react';
 import gamesData from "/public/JSON/games.json";
+import { useRouter } from 'next/navigation';
 
 export default function Games() {
   const [gameIcons, setGameIcons] = useState([]);
+  const [show, setShow] = useState(false);
+  const [currGame, setCurrGame] = useState("");
+  const router = useRouter();
 
   useEffect(() => {
     const arr = Object.values(gamesData).map(data => (
-      <Image key={data.title} src={data.image} alt={`Icon of game: ${data.title}`} width={256} height={256}/>
+      <Image 
+        key={data.title} src={data.image} 
+        alt={`Icon of game: ${data.title}`} width={256} height={256}
+        onClick={_=>{setShow(true); setCurrGame(data.title); router.push(`/rooms/games?game=${data.id}`)}}
+      />
     ));
     setGameIcons(arr); 
   }, [])
@@ -25,6 +33,12 @@ export default function Games() {
         alt={"Image of a TV with the nostalgic Windows XP background."}
       />
       <div className="gamesContainer">
+        {show && 
+          <div className='gamePopUp'>
+          <button onClick={_=>{setShow(false)}}>X</button>
+          <p>{`HELLO i am the game: ${currGame}`}</p>
+          </div>
+        }
         <div className='gamesGrid'>
           {gameIcons}
         </div>
